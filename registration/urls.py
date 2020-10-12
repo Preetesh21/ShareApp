ad98e8b7040c323f6f411ec47036a391900537ad
Preetesh Verma <verma.preetesh21@gmail.com>
2020-10-12 23:17:16 +0530

    changes with the latest django update

--- registration/urls.py
+++ registration/urls.py
@@ -8,6 +8,6 @@ urlpatterns=[
    # path('success',views.success,name='success'),
     path('profile',views.profile,name='profile'),
     #path('make_profile',views.make_profile,name='make_profile'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     views.activate, name='activate'),
 ]
