3929acaea108d6e70766abcc666a5a2c5692f90c
Preetesh21 <50336851+Preetesh21@users.noreply.github.com>
2021-11-22 16:21:36 +0530

    Update settings.py

--- Django/albert/settings.py
+++ Django/albert/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wq7-$v923h3dr(w%%n!pcfj1&6e_s_1fq2-8krf1pe5+7!pp2^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
